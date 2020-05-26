77cd636528d612bbeffe0f7947b95509561280d0
yckao <yckaokarl@gmail.com>
2020-05-27 02:17:28 +0900

    fix: created table before read

--- src/pg-fs.ts
+++ src/pg-fs.ts
@@ -20,11 +20,14 @@ export default class PGPackageManager implements IPGPackageManager {
   public logger: Logger;
 
   private sql: postgres.Sql<never>;
+  private table: Promise<string>;
 
   public constructor(sql: postgres.Sql<never>, prefix: string, logger: Logger) {
     this.sql = sql;
     this.prefix = prefix;
     this.logger = logger;
+
+    this.table = this._createTable(TABLE_NAME);
   }
 
   public async updatePackage(
@@ -64,6 +67,7 @@ export default class PGPackageManager implements IPGPackageManager {
       callback(err);
     }
   }
+
   public async removePackage(callback: (err: NodeJS.ErrnoException | null) => void): Promise<void> {
     this.logger.debug({ packageName: this.prefix }, '[pg-storage/removePackage] remove a package: @{packageName}');
 
@@ -173,6 +177,7 @@ export default class PGPackageManager implements IPGPackageManager {
   }
 
   private async _createFile(name: string, contents: string, callback: Function): Promise<void> {
+    await this.table;
     this.logger.trace({ name }, '[pg-storage/_createFile] create a new file: @{name}');
     try {
       this._readStorageFile(name);
@@ -187,6 +192,7 @@ export default class PGPackageManager implements IPGPackageManager {
   }
 
   private async _readStorageFile(name: string): Promise<Buffer> {
+    await this.table;
     this.logger.trace({ name }, '[pg-storage/_readStorageFile] read a file: @{name}');
 
     try {
@@ -219,14 +225,8 @@ export default class PGPackageManager implements IPGPackageManager {
   }
 
   private async _writeFile(dest: string, data: Buffer, cb: Callback = noop): Promise<Error | null> {
+    await this.table;
     try {
-      await this.sql`
-        CREATE TABLE IF NOT EXISTS ${this.sql(TABLE_NAME)} (
-        path TEXT PRIMARY KEY,
-        content bytea,
-        created_at timestamp not null default current_timestamp,
-        updated_at timestamp not null default current_timestamp
-      )`;
       await this.sql.begin(async sql => {
         await sql`
           INSERT INTO ${sql(TABLE_NAME)} (path, content, updated_at) VALUES (${dest}, ${data}, NOW())
@@ -242,14 +242,27 @@ export default class PGPackageManager implements IPGPackageManager {
   }
 
   private async _deleteFile(name: string): Promise<void> {
+    await this.table;
     await this.sql`
       DELETE FROM ${this.sql(TABLE_NAME)} WHERE path = ${name}
     `;
   }
 
   private async _deletePrefix(prefix: string): Promise<void> {
+    await this.table;
     await this.sql`
       DELETE FROM ${this.sql(TABLE_NAME)} WHERE path = ${prefix + '%'}
     `;
+  }
+
+  private async _createTable(name): Promise<string> {
+    await this.sql`
+        CREATE TABLE IF NOT EXISTS ${this.sql(name)} (
+        path TEXT PRIMARY KEY,
+        content bytea,
+        created_at timestamp not null default current_timestamp,
+        updated_at timestamp not null default current_timestamp
+      )`;
+    return name;
   }
 }
